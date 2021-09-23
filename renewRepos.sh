3731361287e055487b32ab8f498f2b273455e1b4
blurdel <blurdel@gmail.com>
2021-09-23 11:30:11 -0400

    Update renewRepos.sh

--- renewRepos.sh
+++ renewRepos.sh
@@ -4,19 +4,22 @@
 
 for d in * ; do
 
-    if [[ -d ${d} ]]; then
-        echo "${d}"
-        cd "${d}"
-        #ls
-        if [[ -d ".git" ]]; then
-            echo "[ .git repo ]"
-            rm -rf .git
-            git init .
-            git add . && git commit -a -m"initial commit"
-            git branch -M main
-        fi
-        cd - 2>&1 > /dev/null
-        echo
+  if [[ -d ${d} ]]; then
+    cd "${d}"
+
+    # Check if this is a git repo, if it is, re-init it
+    if [[ -d ".git" ]]; then
+      echo "${d} -> [ .git repo ]"
+      rm -rf .git
+      git init .
+      git add . && git commit -am "initial commit"
+      git branch -M main
+    else
+      # Juest echo the directory name
+      echo "${d}"
     fi
+    cd - 2>&1 > /dev/null
+    echo
+  fi
 
 done
